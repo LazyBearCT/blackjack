49fe4259a4b800914b618472d706c81d13874883
LazyBearCT <alex.mayshev.86@gmail.com>
2021-01-30 11:17:24 +0300

    add Hand

--- src/ru/mayalex/blackjack/deck/Hand.java
+++ src/ru/mayalex/blackjack/deck/Hand.java
@@ -1,2 +1,35 @@
-package ru.mayalex.blackjack.deck;public class Hand {
+package ru.mayalex.blackjack.deck;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+
+    private List<Card> cards = new ArrayList<>();
+
+    public void clear() {
+        cards.clear();
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public int getTotal() {
+        if (cards.isEmpty() || cards.get(0).getValue() == 0) {
+            return 0;
+        }
+        int total = 0;
+        boolean isContainsAce = false;
+        for (Card card : cards) {
+            total += card.getValue();
+            if (card.getRank() == Rank.ACE) {
+                isContainsAce = true;
+            }
+        }
+        if (isContainsAce && total <= 11) {
+            total += 10;
+        }
+        return total;
+    }
 }
