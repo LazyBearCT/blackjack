59d7d0766cb6e381e3c0487d534defabb0fb86b0
LazyBearCT <alex.mayshev.86@gmail.com>
2021-01-30 10:28:02 +0300

    add Rank

--- src/ru/mayalex/blackjack/Main.java
+++ src/ru/mayalex/blackjack/Main.java
@@ -1,4 +1,8 @@
 package ru.mayalex.blackjack;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/ru/mayalex/blackjack/deck/Rank.java
@@ -0,0 +1,34 @@
+package ru.mayalex.blackjack.deck;
+
+public enum Rank {
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("J", 10),
+    QUEEN("Q", 10),
+    KING("K", 10),
+    ACE("A", 1);
+
+    private String name;
+    private int value;
+
+    Rank(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
